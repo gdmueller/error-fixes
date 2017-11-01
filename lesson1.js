0dba1f96bab58ea0d551750be2110e909e2a2d89
Peter Horton <phorton@softstackfactory.org>
2017-11-01 04:29:40 +0000

    fixed lesson1 and 2

--- lesson1.js
+++ lesson1.js
@@ -1,4 +1,6 @@
-let names = ["pedro", "john"]
+//Should console.log all names in the name array
+
 for (var i = 0; i < names.length; i++) {
     console.log("My Name is " + names[i])
 }
+let names = ["pedro", "john"]
--- lesson2.js
+++ lesson2.js
@@ -1,4 +1,6 @@
+//Should console.log all names in the name array
+
 var names = ["Alibaba", "Cassandra", "AJ"]
-for (var i = 0; i < names.length; i++) {
+for (var i = 0; i <= names.length; i++) {
     console.log("My Name is " + names[i])
 }
