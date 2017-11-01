8dd70893264bdb9c2eaa372b820f60770617ec2e
Peter Horton <phorton@softstackfactory.org>
2017-11-01 01:40:44 +0000

    fixed == to !==

--- lesson13.js
+++ lesson13.js
@@ -1,21 +1,23 @@
 //Disired Result filteredNames does not have "Roger in it"
 
-var people = [ 
-        { name: "John", age: 55 }, 
-        { name: "Roger", age: 44 },
-        { name: "Betty", age: 37 }
+var people = [
+    { name: "John", age: 55 },
+    { name: "Roger", age: 44 },
+    { name: "Betty", age: 37 }
     ]
 
 
-var nameFilter = function(filteredName){
+var nameFilter = function(filteredName) {
     var results = []
-    for ( var i = 0; i < people.length; i++){
-       let currentName = people[i].name
-        if (currentName != filteredName) {
-        results.push(filteredName)
+    for (var i = 0; i < people.length; i++) {
+        let currentName = people[i].name
+        if (currentName !== filteredName) {
+            results.push(filteredName)
         }
     }
     return results
 }
+
+//Error is Above
 var filteredNames = nameFilter("Roger")
 console.log(filteredNames)
